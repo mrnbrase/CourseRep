1f107cd92b141f4e537b376e848f6a0da4bfb1e9
mrnbrase <mrnbrase@hotmail.com>
2024-04-27 13:32:40 -0400

    adding grocert cal

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -3,6 +3,15 @@ function calculateArea() {
     width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+function groceryTracker() {
+
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let totalamt = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerText = `The total amount is: $${totalamt}`;
+}

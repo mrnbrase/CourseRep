e031e0bd7755c36492c052ad43787fc0bb89d9d4
mrnbrase <mrnbrase@hotmail.com>
2024-04-28 16:21:43 -0400

    adding a delet for the list

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -13,19 +13,25 @@ function addBook() {
             pagesNumber: pagesNumber
         };
         books.push(book);
-        showbooks();
+        showBooks();
         clearInputs();
     } else {
         alert('Please fill in all fields correctly.');
     }
 }
 
-function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-        <p><strong>Book Name:</strong> ${book.name}</p>
-        <p><strong>Author Name:</strong> ${book.authorName}</p>
-        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>`).join('');
+function showBooks() {
+    const booksDiv = books.map((book, index) => `
+        <div>
+            <h1>Book Number: ${index + 1}</h1>
+            <p><strong>Book Name:</strong> ${book.name}</p>
+            <p><strong>Author Name:</strong> ${book.authorName}</p>
+            <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+            <p><strong>No. of Pages:</strong> ${book.pagesNumber} mins</p>
+            <button onclick="deleteBook(${index})">Delete</button>
+        </div>
+    `).join('');
+        
     document.getElementById('books').innerHTML = booksDiv;
 }
 
@@ -34,4 +40,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index) {
+    books.splice(index, 1);
+    showBooks(); // Refresh the displayed book list
 }
